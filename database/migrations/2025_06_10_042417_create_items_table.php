c0ee9cb68721a6cd05b10a3ffa4430dccc86db01
AgathaFlorencia <florenciaagatha05@gmail.com>
2025-06-10 11:28:26 +0700

    items table

--- database/migrations/2025_06_10_042417_create_items_table.php
+++ database/migrations/2025_06_10_042417_create_items_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
+            $table->string('photo')->nullabale();
+            $table->string('name');
+            $table->integer('price');
+            $table->integer('stock');
             $table->timestamps();
         });
     }
