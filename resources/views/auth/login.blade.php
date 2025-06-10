e73271bd239a2b344055ca1bf9f86692538636b6
AgathaFlorencia <florenciaagatha05@gmail.com>
2025-06-10 12:00:14 +0700

    halaman login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body>
+<div class="container mt-4">
+    <h2>Login</h2>
+    
+    @if ($errors -> any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error}}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ url('/login') }}" method="POST">
+        @csrf
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" name="email" id="email" class="form-control" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" name="password" id="password" class = "form-control" required>
+        </div>
+
+        <button type="submit" class="btn btn-success">Login</button>
+        <a href="{{ route('signup') }}" class="btn btn-secondary">Daftar</a>
+    </form>
+</div>
+</body>
+</html>
+
